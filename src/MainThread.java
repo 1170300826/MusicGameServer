5dfe58143d7bd012afb687e83d77bce4cf5c8343
hhldx <hahalidaxin@163.com>
2018-05-16 22:44:21 +0800

    5.16 initial socket network is doing

--- src/MainThread.java
+++ src/MainThread.java
@@ -1,14 +1,12 @@
-import com.sun.security.ntlm.Server;
-
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class MainThread extends Thread{
-    public static final String CLIENT_URL = 172.20.70.121;
     public static final int CLIENT_PORT = 9999;
     public static final Object lock = new Object(); //主线程访问的锁
 
+    public static MainThread mainThread ;
     boolean flag = true;
     ServerSocket ss;
 
@@ -18,7 +16,7 @@ public class MainThread extends Thread{
     @Override
     public void run() {
         try {
-            ss = new ServerSocket(CLIENT_URL,CLIENT_PORT);
+            ss = new ServerSocket(CLIENT_PORT);
             System.out.println("监听到9999端口....");
 
         } catch(Exception e) {
@@ -30,7 +28,8 @@ public class MainThread extends Thread{
                 System.out.println(sc.getInetAddress()+" 已经连接");
                 clientCount += 1;
                 //建立新的serveAgent进行处理用户相关的处理
-                ServerAgent serverAgent = new ServerAgent();
+                ServerAgent serverAgent = new ServerAgent(sc);
+                serverAgent.start();
                 listClient.add(serverAgent);
             } catch(Exception e) {
                 e.printStackTrace();
@@ -39,7 +38,10 @@ public class MainThread extends Thread{
     }
 
     public static void main(String[] args) {
-        new MainThread().start();       //开启总线程MainThread
+        if(mainThread==null) {
+            mainThread = new MainThread();
+            mainThread.start();       //开启总线程MainThread
+        }
     }
 
 }
--- src/ServerAgent.java
+++ src/ServerAgent.java
@@ -1,10 +1,38 @@
-//单独一个clietn的thread用来处理相关的处理操作
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+//单独一个client的thread用来处理相关的处理操作
 public class ServerAgent extends Thread{
     boolean flag = true;
+    Socket sc;
+    DataInputStream din;
+    DataOutputStream dout;
+
+    public ServerAgent (Socket sc) {
+        this.sc = sc;
+        try{
+            din = new DataInputStream(sc.getInputStream());
+            dout = new DataOutputStream(sc.getOutputStream());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void run() {
         while(flag) {
-
+            try {
+                System.out.println("1");
+                String msg = din.readUTF();
+                System.out.println("2");
+                System.out.println(msg);
+            } catch(Exception e) {
+                //已经退出了当前的serverAgent
+                System.out.println("客户端已经退出");
+                // TODO: 2018/5/16 需要在这里进行广播
+                //e.printStackTrace();
+                flag = false;
+            }
         }
     }
 }
